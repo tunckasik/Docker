d0373ad23965f2b70ce23b3165f6f41e8619b44e
tunckasik <tunckasikali@gmail.com>
2023-01-09 14:46:38 -0500

    updating app.py

--- 1_SimpleWebApp/app.py
+++ 1_SimpleWebApp/app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def main():
     return "Welcome!"
 
+@app.route('/how are you')
+def hello():
+    return 'I am good, how about you?'
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)

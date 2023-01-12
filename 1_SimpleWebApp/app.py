e65464a5db4f17c5dd19be3cdb99bf0508a1d0ee
Mehmet Ali TUNCKASIK <101068723+tunckasik@users.noreply.github.com>
2023-01-12 09:43:19 -0500

    Updating app.py > Project1

--- 1_SimpleWebApp/app.py
+++ 1_SimpleWebApp/app.py
@@ -1,14 +1,23 @@
 import os
 from flask import Flask
 app = Flask(__name__)
-
+mysql = MySQL()
+mysql_database_host = 'MYSQL_DATABASE_HOST' in os.environ and os.environ['MYSQL_DATABASE_HOST'] or  'localhost'
+# MySQL configurations
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = ''
+app.config['MYSQL_DATABASE_DB'] = 'shop'
+app.config['MYSQL_DATABASE_HOST'] = mysql_database_host
+mysql.init_app(app)
+conn = mysql.connect()
+cursor = conn.cursor()
 @app.route("/")
 def main():
     return "Welcome!"
 
-@app.route('/how are you')
+@app.route('/bronze')
 def hello():
-    return 'I am good, how about you?'
+    return 'Welcome to Bronze Clouds'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
